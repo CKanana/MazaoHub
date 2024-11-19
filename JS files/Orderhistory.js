6b6a5fa68067bb3fd3a18684b639b612b84f4b61
TMG-prog <tracymuguregitau@gmail.com>
2024-11-19 12:51:10 +0300

    made a few changes to facilitate node js

--- JS files/Orderhistory.js
+++ JS files/Orderhistory.js
@@ -1,21 +1,34 @@
 var orderHistory = JSON.parse(localStorage.getItem('orderHist')) || [];
 
 // Display order details on the page
-var orderHistoryContainer = document.getElementByClassName('Order');
+var orderHistoryContainer = document.getElementById('Orders-sec').getElementsByClassName('Orders-inner')[0];
 if (orderHistory.length === 0) {
-    orderHistoryContainer.innerText = 'No orders have been placed yet.';
+    orderHistoryContainer.innerHTML = '<p>No orders have been placed yet.</p>';
 } else {
     orderHistory.forEach(function (orderItem) {
-        var order = getElementByClassName('Order')
-        var orderItemEl = order.createElement('div');
+        // Create a new div for each order item
+        var orderItemEl = document.createElement('div');
+        orderItemEl.id = ('Order-item-sec')
         orderItemEl.classList.add('order-item');
-        orderItemEl.innerHTML =
-            `
-                    <h2>${orderItem.title}</h2>
-                    <p>Price: ${orderItem.price}</p>
-                    <p>Quantity: ${orderItem.quantity}</p>
-                    <hr>
-                `
+        
+        // Set the inner HTML of the order item
+        orderItemEl.innerHTML = `
+            <h4 style=''>${orderItem.title}</h4>
+            <p>Price: ${orderItem.price}</p>
+            <p>Quantity: ${orderItem.quantity}</p>
+            <p id="total">Total:${(orderItem.price)*(orderItem.quantity)}
+        `;
+        
+        // Append the order item to the container
         orderHistoryContainer.appendChild(orderItemEl);
+        var sokobutton = document.getElementById('Soko-but')
+        sokobutton.remove()
     });
 }
+
+function sokoPage() {
+    window.location.href = 'Soko.html';
+}
+
+var sokobutton = document.getElementById('Soko-but');
+sokobutton.addEventListener('click', sokoPage);
