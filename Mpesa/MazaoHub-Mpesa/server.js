149ffeb007085e6050de8632a2487887e4029921
CKanana <crystal.kanana@strathmore.edu>
2024-11-23 19:00:53 +0300

    final mpesa

--- Mpesa/MazaoHub-Mpesa/server.js
+++ Mpesa/MazaoHub-Mpesa/server.js
@@ -1,14 +1,20 @@
-import express from 'express';
-import cors from 'cors';
-import 'dotenv/config';
-import { router } from './routes/mpesa.routes.js';
+import express from "express";
+import cors from "cors";
+import 'dotenv/config'
 
-const app = express();
+// initialize express
+const app = express()
 
-app.use(express.json());
-app.use(cors());
-app.use(express.static('public'));
-app.use('/api', router);
+// middlewares
+app.use(express.json())
+app.use(cors())
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Server running on port ${port}`));
+// import routes
+import lipaNaMpesaRoutes from "./routes/routes.lipanampesa.js"
+app.use('/api',lipaNaMpesaRoutes)
+
+const port = process.env.PORT
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`)
+})
